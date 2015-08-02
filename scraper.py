eee86de807575704968928c822ab389fcc44231d
blablupcom <o.volodin@yahoo.com>
2015-08-02 15:52:38 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,5 +2,5 @@ import requests
 from bs4 import BeautifulSoup as bs
 p = requests.get('http://www.manta.com/world/Oceania/Australia/')
 s = bs(p.text)
-title = s.find('span', attrs={'itemprop':'title').text
+title = s.find('span', attrs={'itemprop':'title'}).text
 print title
