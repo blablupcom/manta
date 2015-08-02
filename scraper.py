bb2e0d153a75ecd1601e99b5897cade7473353a9
blablupcom <o.volodin@yahoo.com>
2015-08-02 16:10:59 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 import urllib2
-import requests
+import unirest
 from bs4 import BeautifulSoup as bs
 user_agent = {'User-Agent': 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
-p = requests.get('http://www.manta.com/world/Oceania/Australia/', headers=user_agent)
+p = unirest.get('http://www.manta.com/world/Oceania/Australia/', headers=user_agent)
 s = bs(p)
 title = s.find('span', attrs={'itemprop':'title'}).text
 print title
