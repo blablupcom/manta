3cce7be869384f86dd4e608f75bcf8c08743cb1a
blablupcom <o.volodin@yahoo.com>
2015-08-02 16:12:28 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import urllib2
-import unirest
+import requests
 from bs4 import BeautifulSoup as bs
 user_agent = {'User-Agent': 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
 p = requests.get('http://www.manta.com/world/Oceania/Australia/', headers=user_agent)
