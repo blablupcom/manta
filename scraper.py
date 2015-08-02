16256ade5cd87077c76159fd85b8a0f2b59c8208
blablupcom <o.volodin@yahoo.com>
2015-08-02 16:13:46 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,6 @@ from bs4 import BeautifulSoup as bs
 user_agent = {'User-Agent': 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
 p = requests.get('http://www.manta.com/world/Oceania/Australia/', headers=user_agent)
 s = bs(p.text)
-print soup
+print s
 title = s.find('span', attrs={'itemprop':'title'}).text
 print title
