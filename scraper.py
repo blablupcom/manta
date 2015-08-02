d8ee74f2ac64a3c543364e3afe2b2e0164dc0ec2
blablupcom <o.volodin@yahoo.com>
2015-08-02 16:06:41 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import urllib2
 from bs4 import BeautifulSoup as bs
-p = urllib2.urlopen('http://www.manta.com/world/Oceania/Australia/')
+p = requests.get('http://www.manta.com/world/Oceania/Australia/')
 s = bs(p)
 title = s.find('span', attrs={'itemprop':'title'}).text
 print title
